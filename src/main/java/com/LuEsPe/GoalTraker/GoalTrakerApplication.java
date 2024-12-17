47686d7182852297eae7777ec571bb1002c7e0c1
LuEsPe <luciaesteban.desarrollo@gmail.com>
2024-12-17 21:59:43 +0100

    Initial commit

---
+++ src/main/java/com/LuEsPe/GoalTraker/GoalTrakerApplication.java
@@ -0,0 +1,13 @@
+package com.LuEsPe.GoalTraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalTrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalTrakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LuEsPe/GoalTraker/GoalTrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LuEsPe.GoalTraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
